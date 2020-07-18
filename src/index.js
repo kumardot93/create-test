a9084f755e2a13de782db5505fec372cca248bf3
aditya <kumar.93@iitj.ac.in>
2020-07-18 07:34:44 +0530

    adjustd to new media container azure

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import SocketManager from './SocketManager.js';
 window.base = 'http://localhost:8000';
 window.hostName = 'localhost:8000'; //For web socket connection
 window.media_url = window.base + '/media/';
-// window.media_url = 'https://eduhub.blob.core.windows.net/eduhub/'; //For production
+// window.media_url = 'https://cadmea.blob.core.windows.net/education/'; //For production
 window.cred = 'include'; //change to same-origin for production
 
 ReactDOM.render(
